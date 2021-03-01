8f902f0a6d875e892132e8204a628115970fb10b
Ozymandias <tomas.rav@gmail.com>
2021-03-01 04:14:18 +0200

    - SMIL animation fixes for Chrome

--- src/app/interface/sidebar-props/sidebar-props.component.ts
+++ src/app/interface/sidebar-props/sidebar-props.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, Input, ViewChild, ElementRef } from "@angular/core";
 
 import { Observable } from "../../types/observer";
-import { SavageSVG } from "../../types/svg";
+import { SavageSVG, findParent } from "../../types/svg";
 import { HistoryService } from "../../services/history.service";
 import { HotkeyService } from "../../services/hotkey.service";
+import { SvgFileService } from "../../services/svg-file.service";
 
 
 const MDN_URI = "https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute";
@@ -27,13 +28,16 @@ export class SidebarPropsComponent implements OnInit {
 	set cls(val: string) {
 		this.selection.forEach((e) => e.attributes.class = val);
 	}
+	document: Observable<SavageSVG>;
 
 	constructor(
 		public history: HistoryService,
 		public hotkey: HotkeyService,
+		public file: SvgFileService,
 	) { }
 
 	ngOnInit(): void {
+		this.file.openFile.subscribe((file) => this.document = file);
 		this.hotkey.triggered.subscribe((key) => {
 			switch (key) {
 				case "properties":
@@ -49,7 +53,17 @@ export class SidebarPropsComponent implements OnInit {
 		window.open(`${MDN_URI}/${attr}`, "_blank");
 	}
 
-	edited(): void {
+	edited(animation?: boolean): void {
+		if (animation) {
+			// Chrome bug workaround. If animation with no attributes is added
+			// animation doesn't start when attributes are edited
+			// We need to remove and re-add the whole animation element
+			// But this is a big performance hit
+			const parent = findParent(this.document, this.selection[0].nid);
+			const index = parent.children.indexOf(this.selection[0]);
+			parent.children.splice(index, 1);
+			parent.children.splice(index, 0, this.selection[0]);
+		}
 		this.history.snapshot("Property edited");
 	}
 }
